b3561fdbd8a94675bcff2eb6f3a4de5128447e38
Depdog <89854913+Depdog@users.noreply.github.com>
2023-05-18 20:41:42 -0400

    addding main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from pytube import YouTube
+
+app = Flask('app')
+
+
+@app.route('/', methods=['GET'])
+def show_indexhtml():
+  return render_template('index.html')
+
+
+@app.route('/send_data', methods=['POST'])
+def get_data_from_html():
+  yt = YouTube(request.form['pay'])
+  ytob = yt.streams.get_by_itag(22).url
+  return f"""
+  <head>
+    <title>Video</title>
+  </head>
+  <body>
+    <center>
+      <video width='100%' height='100%' controls> 
+        <source src='{ytob}' type='video/mp4'>  
+      </video>
+    </center>
+  </body>
+  """
+
+
+app.run(host='0.0.0.0', port=8080)
